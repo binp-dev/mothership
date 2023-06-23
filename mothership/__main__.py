d36822cc4c05f747798baaf0212f86cb25d6d827
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-06-23 09:47:11 +0700

    CLI instead of service

--- mothership/__main__.py
+++ mothership/__main__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from pathlib import Path
-from time import sleep
 from argparse import ArgumentParser
 import toml
 
@@ -11,31 +10,35 @@ from .overlayfs import Overlayfs
 
 
 parser = ArgumentParser(prog="Mothership", description="PS controller orchestration")
-parser.add_argument("config_path", type=str, help="Path to config.toml")
+parser.add_argument(
+    "command",
+    type=str,
+    choices=["up", "down"],
+    help="Command to run.",
+)
+parser.add_argument(
+    "config",
+    nargs="?",
+    default=None,
+    type=str,
+    help="Path to config.toml",
+)
 args = parser.parse_args()
-with open(Path(args.config_path), "r") as f:
-    config = Config(**toml.load(f))
-print(config)
 
 assert FS_PATH.exists(), f"Base FS directory not found at {FS_PATH}"
 nfs = Nfs()
 overlayfs = Overlayfs()
 
-try:
-    print("Starting mothership service")
+if args.command == "up":
+    assert args.config is not None
+    with open(Path(args.config), "r") as f:
+        config = Config(**toml.load(f))
+    print(config)
+
     nfs.unexport()
     overlayfs.mount(config)
     nfs.export(config)
 
-    print("Mothership service started")
-    while True:
-        sleep(0.1)
-
-except Exception as e:
-    print("Error:", e)
-    raise
-
-finally:
+elif args.command == "down":
     nfs.unexport()
     overlayfs.unmount()
-    print("Mothership service stopped")
