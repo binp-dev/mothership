1fb3b296b6c9c27e716c9586bfe9265290ac5e42
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-04 10:38:53 +0700

    Monitor runner

--- mothership/monitor/__init__.py
+++ mothership/monitor/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from typing import Any, Awaitable
 
 import asyncio
 from pathlib import Path
+from dataclasses import dataclass
 
 import toml
 from flask import Flask
@@ -9,22 +11,33 @@ from flask import Flask
 from mothership.config import Config
 
 
-_CONFIG: Config | None = None
+@dataclass
+class Context:
+    config: Config
 
 
-def config() -> Config:
-    global _CONFIG
-    if _CONFIG is None:
-        with open(Path("config.toml"), "r") as f:
-            config = Config(**toml.load(f))
-        print(config)
-        _CONFIG = config
-    return _CONFIG
+CONTEXT: Context
+
+
+def ctx() -> Context:
+    global CONTEXT
+    return CONTEXT
 
 
 app = Flask(__name__)
 
 
+async def start(serve: Awaitable[None]) -> None:
+    with open(Path("config.toml"), "r") as f:
+        config = Config(**toml.load(f))
+    print(config)
+
+    global CONTEXT
+    CONTEXT = Context(config)
+
+    await serve
+
+
 @app.route("/")
-def root() -> str:
-    return str(config())
+async def root() -> str:
+    return str(ctx().config)
