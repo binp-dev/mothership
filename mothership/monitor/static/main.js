548ed07e4e1a31e0366386a59def0b51c571b199
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-06 10:00:32 +0700

    Monitor: show hosts info

--- mothership/monitor/static/main.js
+++ mothership/monitor/static/main.js
@@ -1,5 +1,5 @@
 const main = () => {
-    request(console.log);
+    request(render);
 }
 
 const request = (callback) => {
@@ -20,4 +20,39 @@ const request = (callback) => {
     xhr.send(null);
 }
 
-document.body.onload = main;
+const render = (hosts) => {
+    const root = document.getElementById("hosts");
+    for (const mac in hosts) {
+        const host = hosts[mac]
+        let elem = document.getElementById(mac);
+        if (elem === null) {
+            elem = document.createElement("div");
+            elem.id = mac;
+            elem.classList.add("host");
+            root.appendChild(elem);
+        }
+        update_host_element(elem, mac, host);
+    }
+}
+
+const update_host_element = (elem, mac, host) => {
+    html = `<div class="mac">MAC: ${mac}</div>`;
+    if (host.device !== undefined && host.device !== null) {
+        html += `<div>Name: ${host.device.name}</div>`
+        html += `<div>Base: ${host.device.base}</div>`;
+    }
+    if (host.info !== undefined && host.info !== null) {
+        html += `<div>IP: ${host.info.addr}</div>`
+        html += `<div>Boot: ${seconds_to_date(host.info.boot)}</div>`;
+        html += `<div>Online: ${seconds_to_date(host.info.online)}</div>`;
+    }
+    elem.innerHTML = html;
+}
+
+const seconds_to_date = (seconds) => {
+    let date = new Date(0);
+    date.setUTCSeconds(seconds);
+    return date;
+}
+
+window.onload = main;
