716100ef681b23186448c774c10da604b62a1cbf
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-13 15:11:28 +0700

    Add devices in crate to config

--- config.py
+++ config.py
@@ -9,17 +9,92 @@ skifio = Base("skifio", tornado)
 hosts = [
     Host(
         Mac("f8:dc:7a:46:04:58"),
-        base=skifio,
-        files={Path("/opt/env.cmd"): 'epicsEnvSet("NAME","tornado0")\n'},
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=tornado0\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1b:34"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc13\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:42"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc14\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:60"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc15\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:9c"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc16\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:a6"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc17\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:cc"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc18\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:de"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc19\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:e0"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc20\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:e2"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc21\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:e4"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc22\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:e6"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc23\n"},
     ),
     Host(
         Mac("f8:dc:7a:a4:1c:e8"),
         base=skifio,
-        files={Path("/opt/env.cmd"): 'epicsEnvSet("NAME","mpsc25")\n'},
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc24\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:ec"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc25\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:ee"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc26\n"},
     ),
     Host(
         Mac("f8:dc:7a:a4:1c:f2"),
-        base=skifio,
-        files={Path("/opt/env.cmd"): 'epicsEnvSet("NAME","mpsc27")\n'},
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc27\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1c:f4"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc28\n"},
+    ),
+    Host(
+        Mac("f8:dc:7a:a4:1d:16"),
+        base=tornado,
+        files={Path("/opt/env.cmd"): "export DEV_NAME=mpsc29\n"},
     ),
 ]
