8484f67da8dd4bd6958d5033fb3f69ee5ebe291a
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-26 11:38:26 +0700

    Format info in window

--- mothership/monitor/static/script/hosts.js
+++ mothership/monitor/static/script/hosts.js
@@ -1,10 +1,11 @@
-import { seconds_to_date, is_now, is_recent, format_date } from "./utils.js";
+import { seconds_to_date, is_now, is_recent, format_date, format_date_relative, compact_date_html } from "./utils.js";
 import { CONTEXT } from "./context.js";
 import { navigate } from "./navigate.js"
 
 export const render = () => {
     const root = document.getElementById("hosts");
     const hosts = CONTEXT.hosts;
+
     for (const mac in hosts) {
         const host = hosts[mac]
         let elem = document.getElementById(mac);
@@ -17,11 +18,13 @@ export const render = () => {
         }
         update_host_tile(elem, mac, host);
     }
+
     for (const elem of Array.from(root.childNodes)) {
         if (hosts[elem.id] === undefined) {
             root.removeChild(elem);
         }
     }
+
     update_host_window();
 }
 
@@ -40,10 +43,10 @@ const update_host_tile = (elem, mac, host) => {
     if (host.status !== undefined && host.status !== null) {
         const boot = seconds_to_date(host.status.boot);
         html += `<div>${host.status.addr}</div>`
-        html += `<div>Booted: ${format_date(boot)}</div>`;
+        html += `<div>Booted: ${compact_date_html(boot)}</div>`;
         const online = seconds_to_date(host.status.online);
         if (!is_now(online)) {
-            html += `<div>Last seen: ${format_date(online)}</div>`;
+            html += `<div>Last seen: ${compact_date_html(online)}</div>`;
             elem.classList.add("error");
             if (is_recent(online)) {
                 elem.classList.add("recent");
@@ -74,30 +77,26 @@ export const update_host_window = () => {
         if (host.config !== undefined && host.config !== null) {
             html += `<div>Type: ${host.config.base}</div>`;
         } else {
-            html += `<div>Not in config</div>`;
+            html += `<div><span class="badge err">Not in config</span></div>`;
         }
 
         if (host.status !== undefined && host.status !== null) {
             const boot = seconds_to_date(host.status.boot);
-            html += `<div>${host.status.addr}</div>`
-            html += `<div>Booted: ${format_date(boot)}</div>`;
-            html += `<button class="reboot">Reboot</button>`;
+            html += `<div>Address: ${host.status.addr}</div>`
+            html += `<div>Booted: ${format_date_relative(boot)} (${format_date(boot)}) <button class="reboot">Reboot</button></div>`;
             rebootable = true;
             const online = seconds_to_date(host.status.online);
-            if (!is_now(online)) {
-                html += `<div>Last seen: ${format_date(online)}</div>`;
-                if (is_recent(online)) {
-                }
+            if (is_now(online)) {
+                html += `<div>Last seen: <span class="badge ok">Now</span></div>`;
             } else {
-                if (is_recent(boot)) {
-                }
+                html += `<div>Last seen: <span class="badge err">${format_date_relative(online)}</span> (${format_date(online)})</div>`;
             }
 
         } else {
-            html += `<div>Offline</div>`;
+            html += `<div>Status: <span class="badge err">Offline</span></div>`;
         }
     } else {
-        html += `Host '${mac}' not found.`
+        html += `<h2><span class="badge err">Host '${mac}' not found</span></h2>`
     }
 
     elem.innerHTML = html;
--- mothership/monitor/static/script/utils.js
+++ mothership/monitor/static/script/utils.js
@@ -19,20 +19,31 @@ export const is_recent = (date) => {
 }
 
 export const format_date = (date) => {
-    const full = ("0" + date.getDate()).slice(-2) + "-"
+    return ("0" + date.getDate()).slice(-2) + "-"
         + ("0" + (date.getMonth() + 1)).slice(-2) + "-"
         + date.getFullYear() + " "
         + ("0" + date.getHours()).slice(-2) + ":"
         + ("0" + date.getMinutes()).slice(-2);
+}
+
+export const format_date_relative = (date) => {
+    let m = minutes_passed(date);
+    let h = Math.floor(m / 60);
+    m = m % 60;
+    let d = Math.floor(h / 24);
+    h = h % 24;
+    return (d != 0 ? d + "d " : "") + (h != 0 ? h + "h " : "")
+        + (m != 0 || (d == 0 && h == 0) ? m + "m " : "")
+        + "ago";
+}
+
+export const compact_date_html = (date) => {
+    const full = format_date(date);
     let text = full;
 
     let m = minutes_passed(date);
-    if (m < RECENTLY) {
-        let h = Math.floor(m / 60);
-        m = m % 60;
-        text = (h != 0 ? h + "h " : "")
-            + (m != 0 || h == 0 ? m + "m " : "")
-            + "ago";
+    if (is_recent(date)) {
+        text = format_date_relative(date);
     }
 
     return `<span class="date" title="${full}">${text}</span>`;
